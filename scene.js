d91a3d70e823b91dad6ed8aec4447dcea6ba0bc8
Alexander-Malchev <54078302+Alexander-Malchev@users.noreply.github.com>
2021-08-20 14:46:28 +0300

    updates

--- scene.js
+++ scene.js
@@ -15,7 +15,7 @@ class Scene extends Phaser.Scene {
         super ({key: "Scene"})
     }
     preload () {
-        this.load.spritesheet("player", "assets/player-sheet.png", { frameWidth: 122, frameHeight: 160 })
+        this.load.spritesheet("player", "assets/pl.png", { frameWidth: 875 / 5, frameHeight: 202 })
         this.load.image("p0", "assets/platform[0].png")
         this.load.image("p1", "assets/platform[1].png")
         this.load.image("p2", "assets/platform[2].png")
@@ -41,7 +41,7 @@ class Scene extends Phaser.Scene {
             space: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
             e: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E)
         }
-        this.objects.push( this.add.image (window.innerWidth + 100, 300, `p${random(1, 6)}`));
+        this.objects.push( this.add.image (window.innerWidth + 100, window.innerHeight - 50, `p5`));
     }
     update (delta) {
         if ( this.objects[0].x <= 0 - this.objects[0].width ) {
@@ -49,12 +49,12 @@ class Scene extends Phaser.Scene {
             this.objects.shift()
         }
         if ( this.objects[this.objects.length - 1].x + (this.objects[this.objects.length - 1].width + 50) <= window.innerWidth ) {
-            const image = this.add.image(100000000, 0, `p${random(1, 6)}`)
+            const image = this.add.image(100000000, 0, `p${random(1, 6)}`), y = this.objects[this.objects.length - 1].y + random (-100, 100)
             image.x = window.innerWidth + image.width;
-            image.y = this.objects[this.objects.length - 1].y + random (-100, 100)
+            image.y = y > 150 && y < window.innerHeight - 50 ? y : window.innerHeight - 50
             this.objects.push( image );
         }
-        
+
         for ( let i = 0; i < this.objects.length; i++ ) {
             this.objects[i].x -= 5;
         }
